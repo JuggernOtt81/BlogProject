010295f7361617f39cbb8e338a060e8011a3ddb3
Lawson Ott <34386218+JuggernOtt81@users.noreply.github.com>
2022-09-13 01:22:26 -0400

    HEROKU_POSTGRESQL_CRIMSON_URL

--- Data/DataUtility.cs
+++ Data/DataUtility.cs
@@ -15,7 +15,7 @@ namespace BlogMVC.Data
             var connectionString = configuration.GetConnectionString("DefaultConnection");
 
             //it will be automatically overwritten if running on heroku
-            var databaseUrl = Environment.GetEnvironmentVariable("HEROKU_POSTGRESQL_CRIMSON");
+            var databaseUrl = Environment.GetEnvironmentVariable("HEROKU_POSTGRESQL_CRIMSON_URL");
 
             return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
         }

5e40ed871d0a8198a2e8b260ac4ada9fd2f16046
Lawson Ott <34386218+JuggernOtt81@users.noreply.github.com>
2022-09-12 23:53:54 -0400

    Add project files.

---
+++ Enums/ModerationType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogMVC.Enums
+{
+    public enum ModerationType
+    {
+        [Description("minor safety: if a minor is giving personal info, or if someone is posting about minors doing illegal/violent/sexual acts")]
+        MinorSafety,
+        [Description("propaganda")]
+        Political,
+        [Description("in context only, no word is banned. ex: 'that shit is funny!' is OK, while 'eat shit, dude!' is not")]
+        Language,
+        [Description("promoting/glorifying drug use")]
+        Drugs,
+        [Description("any statement of threat toward any entity")]
+        Threatening,
+        [Description("any graphic or suggestive sexual content")]
+        Sexual,
+        [Description("insults to an ethnicity or a 'call to arms' against an ethnic group")]
+        HateSpeech,
+        [Description("'doxing' or public humiliation of a non-public figure")]
+        Shaming        
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,46 @@
+﻿using BlogMVC.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogMVC.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string BlogUserId { get; set; }
+        public string ModeratorId { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 5)]
+        [Display(Name = "Comment")]
+        public string Body { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Moderated Date")]
+        public DateTime? Moderated { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Deleted Date")]
+        public DateTime? Deleted { get; set; }
+
+        [Display(Name = "Moderated Comment")]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 5)]
+        public string ModeratedBody { get; set; }
+
+        public ModerationType ModerationType { get; set; }
+
+        public virtual Post Post { get; set; }
+        public virtual BlogUser BlogUser { get; set; }
+        public virtual BlogUser Moderator { get; set; }
+    }
+}

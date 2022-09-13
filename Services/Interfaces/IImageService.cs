2ab24f78f970e09bde87cfa9581f84216118abc5
Lawson Ott <34386218+JuggernOtt81@users.noreply.github.com>
2022-09-13 01:06:31 -0400

    Servies & Interfaces

---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+namespace AddressBookMVC.Services.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
